5b8378984f1a0aff77552e371a1204cbc487c2b3
Antoine Flouzat <antoineflouzat@mailbox.org>
2026-02-14 21:04:15 +0100

    inputs

--- player/player.gd
+++ player/player.gd
@@ -10,8 +10,8 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	var direction := Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
 	)
 
 	if direction != Vector2.ZERO:
