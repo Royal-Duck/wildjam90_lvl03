950a9f4831d1093a67ef5ba8000204deb9b8a3bc
Antoine Flouzat <antoineflouzat@mailbox.org>
2026-02-15 22:05:33 +0100

    create enemy ressource

---
+++ npc/enemies/enemy_resource.gd
@@ -0,0 +1,17 @@
+extends Resource
+class_name Enemy
+
+@export_file("*.png", "*.jpg", "*.jpeg", "*.webp") var sprite_path: String = ""
+@export_range(1, 9999, 1) var max_health: int = 10
+@export_range(1, 999, 1) var attack_power: int = 2
+
+var current_health: int = max_health
+
+func take_damage(amount: int) -> void:
+	if amount <= 0:
+		return
+
+	current_health = max(current_health - amount, 0)
+
+func is_dead() -> bool:
+	return current_health <= 0
