b747cbdf4a26361139d6957e362b24772f89de8f
Royal-DUck <geromepont@gmail.com>
2026-02-17 21:41:35 +0100

    slingshot variabler power

--- slingshot/slingshot.gd
+++ slingshot/slingshot.gd
@@ -3,10 +3,11 @@ extends Node2D
 @export var DIST : float = 0;
 @export var COOLDOWN : float = 2.5
 
-# TODO: make a loading system to have stronger shots as the mouse is held
-const power = 200.0
-
 var on_cooldown: float = 0
+var charge_time: float = 0
+
+func power_time(time : float):
+	return clampf(2.0 * 2.0**-((time - 4.0) ** 2.0 / 8.0), 0.5, 4.0) * 300.0
 
 func _process(delta: float) -> void:
 	$basepoint.position = get_local_mouse_position().normalized() * DIST;
@@ -15,11 +16,15 @@ func _process(delta: float) -> void:
 	$basepoint.global_rotation = angle + PI / 2
 	on_cooldown = clampf(on_cooldown - delta, 0.0, COOLDOWN)
 	
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and is_zero_approx(on_cooldown):
+	if Input.is_action_pressed("attack_click") and is_zero_approx(on_cooldown):
+		charge_time += delta
+	if Input.is_action_just_released("attack_click") and is_zero_approx(on_cooldown) and not is_zero_approx(charge_time):
 		on_cooldown = COOLDOWN
 		var rock = preload("res://slingshot/thrown_rock.tscn").instantiate()
 		rock.global_position = $basepoint/launcher.global_position
 		rock.direction = circle_pos
-		rock.speed = power
-		rock.max_travel = 3.0 * power
+		rock.speed = power_time(charge_time)
+		rock.max_travel = power_time(charge_time)
+		charge_time = 0
 		get_parent().get_parent().add_child(rock)
+
