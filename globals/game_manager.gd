0422de74109212af62041a9c5f9d3b2118186faa
Royal-Duck <samueldescours@gmail.com>
2026-02-20 21:13:09 +0100

    fade

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -3,3 +3,4 @@ extends Node
 var has_seen_start_menu: bool = false
 var is_scene_change: bool = false
 var is_retry: bool = false
+var player_position: Vector2 = Vector2.ZERO
--- world.gd
+++ world.gd
@@ -1,21 +1,12 @@
 extends Node2D
 
-const FLASHBACK = preload("uid://brat7n62mmmdh")
-
 @onready var go_to_flash_back_test: Area2D = $GoToFlashBackTest
 @onready var menu: CanvasLayer = $Menu
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	if not menu.visible:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
-	go_to_flash_back_test.body_entered.connect(_on_go_to_flash_back_test_body_entered)
-
-func _on_go_to_flash_back_test_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		GameManager.has_seen_start_menu = true
-		GameManager.is_scene_change = true
-		GameManager.is_retry = false
-		get_tree().call_deferred("change_scene_to_packed", FLASHBACK)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("esc"):
@@ -24,3 +15,14 @@ func _input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		menu.visible = true
 		get_tree().paused = true
+
+
+const FLASHBACK = preload("uid://brat7n62mmmdh")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name != "fade":
+		return;
+	GameManager.has_seen_start_menu = true
+	GameManager.is_scene_change = true
+	GameManager.is_retry = false
+	get_tree().call_deferred("change_scene_to_packed", FLASHBACK)
