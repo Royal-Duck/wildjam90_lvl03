7aaf5cffc156906c29488852d7c4d26a85bcda3b
Antoine Flouzat <antoineflouzat@mailbox.org>
2026-02-22 15:57:52 +0100

    npc animations

--- npc/characters/character_resource.gd
+++ npc/characters/character_resource.gd
@@ -2,16 +2,6 @@ extends Resource
 class_name Character
 
 @export var character_name: String = ""
-@export_file("*.png", "*.jpg", "*.jpeg", "*.webp") var sprite_path: String = ""
 @export_file("*.dialogue") var dialogue_path: String = ""
 @export var dialogue_bubble_text: String = ""
-
-func get_sprite_texture() -> Texture2D:
-	if sprite_path.is_empty():
-		return null
-	return load(sprite_path) as Texture2D
-
-func get_dialogue_resource() -> Resource:
-	if dialogue_path.is_empty():
-		return null
-	return load(dialogue_path)
+@export var sprite_frames: SpriteFrames = null
